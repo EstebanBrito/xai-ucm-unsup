90d92cd675399c9c2734d99dae7601c23647bb74
EstebanBrito <esteban.brito.borges@gmail.com>
2021-10-09 11:03:14 -0500

    Added script to generate class sim matrix based on class probabilities features

--- sim_matrix/sim_funcs.py
+++ sim_matrix/sim_funcs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 
 def choose_sim_function(choice):
     options = list(sim_functions.keys())
@@ -8,9 +9,15 @@ def choose_sim_function(choice):
         raise ValueError("Supported values for sim. functions are:", options)
 
 def euclidean_distance(a, b, is_same_vector=False):
-    if is_same_vector: return 0
+    if is_same_vector: return 0.0
     else: return np.linalg.norm(a-b)
 
+def class_similarity(a, b, is_same_vector=False):
+    if is_same_vector: return 2.0
+    res = a + b
+    return  sqrt(np.sum(res*res))
+
 sim_functions = {
-    'euclidean_distance': euclidean_distance
+    'euclidean_distance': euclidean_distance,
+    'class_similarity': class_similarity
 }
