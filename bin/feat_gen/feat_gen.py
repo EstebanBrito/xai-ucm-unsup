57839b49d511edda9cb611283904a131761aeb75
EstebanBrito <esteban.brito.borges@gmail.com>
2022-02-21 15:21:39 -0600

    Small changes in CLI feat. gen.

--- bin/feat_gen/feat_gen.py
+++ bin/feat_gen/feat_gen.py
@@ -1,16 +1,13 @@
-import os
-
 from .incv1_feats import gen_incv1_feats
 from .incv1_probs import gen_incv1_probs
 from .incv3_feats import gen_incv3_feats
 from .color_hist import gen_color_hist_feats
-from ..utils.wizards import select_feat_gen_params, select_rewrite, YES, NO
-from ..settings.features_and_metrics import FEATURE_OPTIONS
+from ..utils.utils import get_feature_options
 
 def gen_feats(path_input, path_output, feat_opt_id):
-    feat_type_opts = FEATURE_OPTIONS[feat_opt_id]
+    feat_opts = get_feature_options(feat_opt_id)
     # Generate feats. according to user's selected option
-    # TODO: Pass path_input, path_output to functions (remember I/O can change)
+    # TODO: Refactor gen. funcs. into more generic ones (gen_model_based_feats, gen_image_based_feats, etc)
     if feat_opt_id=='incv1latfeats': gen_incv1_feats(path_input, path_output, feat_opt_id)
     elif feat_opt_id=='incv3latfeats': gen_incv3_feats(path_input, path_output, feat_opt_id)
     elif feat_opt_id=='colorhist': gen_color_hist_feats(path_input, path_output, feat_opt_id)
--- bin/scripts/feat_gen_cli.py
+++ bin/scripts/feat_gen_cli.py
@@ -1,5 +1,5 @@
 from ..utils.wizards import select_feat_gen_params, select_rewrite, YES, NO
-from ..settings.features_and_metrics import FEATURE_OPTIONS
+from ..utils.utils import get_feature_options
 from ..settings.folders import IMGS_FOLDER_PATH
 from ..feat_gen import gen_feats
 
@@ -7,7 +7,8 @@ def gen_feats_using_cli():
     print('--- FEATURE GENERATION WIZARD ---')
     feat_opt_id = select_feat_gen_params()
     print()
-    feat_output_file_path = FEATURE_OPTIONS[feat_opt_id]['output_file_path']
+    feat_opts = get_feature_options(feat_opt_id)
+    feat_output_file_path = feat_opts['output_file_path']
     rewrite_opt =  select_rewrite(feat_output_file_path, 'feat_gen')
     if rewrite_opt == YES:
         print('Feature generation WILL BE peformed')
