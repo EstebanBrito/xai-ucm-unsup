Fixed bug in cosine similarity calculation (more similar vectors did not have smaller similarity values)

bin/sim_mx_gen/sim_funcs.py
@@ -11,8 +11,12 @@ def class_similarity(a, b, is_same_vector=False):
     return  sqrt(np.sum(res*res))
 
 def cosine_similarity(a, b, is_same_vector=False):
-    if is_same_vector: return 1.0
-    else: return np.dot(a, b)/(np.linalg.norm(a) * np.linalg.norm(b))
+    # Cosine similarity is defined in [0,1] where bigger values reflect more similarirty
+    # To follow a "less is closer" rule, a complement (1) is substracted from each real cosine distance
+    if is_same_vector: return 0.0
+    else:
+        real_cos_sim = np.dot(a, b)/(np.linalg.norm(a) * np.linalg.norm(b))
+        return 1.0 - real_cos_sim
 
 # TODO: Mkae sure that keys in this dict. always match keys in settings.features_and_metrics.SIM_OPTIONS
 sim_functions = {